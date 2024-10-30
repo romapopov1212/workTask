43b8edbca3fc7b4b42a8a2c3309affa0dbe0bfab
Popov Roman <popovrromma@gmail.com>
2024-10-30 23:56:46 +1000

    add orders get and get by id

--- api/orders.py
+++ api/orders.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter
+from typing import List
+
+from attr import dataclass
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+from models.orders import Order
+from database import get_session
+from db import orders
 
 router = APIRouter(
     prefix='/orders',
 
 )
 
-@router.get('/')
-def get_orders():
-    return []
+@router.get('/', response_model=List[Order])
+def get_orders(session: Session = Depends(get_session)):
+    orders_list = session.query(orders.Order).all()
+    if not orders_list:
+        raise HTTPException(status_code=404, detail="No orders found")
+    return orders_list
 
-@router.get('/{order_id}')
-def get_orders_by_id(id:int):
-    return []
+@router.get('/{order_id}', response_model=Order)
+def get_orders_by_id(order_id:int, session: Session=Depends(get_session)):
+    order_one = session.query(orders.Order).filter(orders.Order.order_id == order_id).first()
+    if order_one is None:
+        raise HTTPException(status_code=404, detail="Order nor found")
+    return order_one
