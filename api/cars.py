046e8c19f0580ee613df2fbc9d1e2c17293ff963
Popov Roman <popovrromma@gmail.com>
2024-10-31 00:14:37 +1000

    small changes

--- api/cars.py
+++ api/cars.py
@@ -9,17 +9,30 @@ router = APIRouter(
     prefix='/cars'
 )
 
+
 @router.get('/', response_model=List[Car])
 def get_cars(session: Session = Depends(get_session)): # внедрение зависимостей
-    cars_list = session.query(cars.Car).all()
+    cars_list = (
+        session
+        .query(cars.Car)
+        .all()
+    )
     if not cars_list:
         raise HTTPException(status_code=404, detail="No cars found")
     return cars_list
 
+
 @router.get('/{car_id}', response_model=Car)
-def get_cars_by_id(car_id: int, session: Session = Depends(get_session)):
-    car_one = session.query(cars.Car).filter(cars.Car.car_id == car_id).first()
+def get_cars_by_id(
+        car_id: int,
+        session: Session = Depends(get_session)
+):
+    car_one = (
+        session
+        .query(cars.Car)
+        .filter(cars.Car.car_id == car_id)
+        .first()
+    )
     if car_one is None:
         raise HTTPException(status_code=404, detail="Car not found")
     return car_one
-
--- api/orders.py
+++ api/orders.py
@@ -1,6 +1,4 @@
 from typing import List
-
-from attr import dataclass
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 
@@ -10,19 +8,31 @@ from db import orders
 
 router = APIRouter(
     prefix='/orders',
-
 )
+
 
 @router.get('/', response_model=List[Order])
 def get_orders(session: Session = Depends(get_session)):
-    orders_list = session.query(orders.Order).all()
+    orders_list = (
+        session
+        .query(orders.Order)
+        .all()
+    )
     if not orders_list:
         raise HTTPException(status_code=404, detail="No orders found")
     return orders_list
 
+
 @router.get('/{order_id}', response_model=Order)
-def get_orders_by_id(order_id:int, session: Session=Depends(get_session)):
-    order_one = session.query(orders.Order).filter(orders.Order.order_id == order_id).first()
+def get_orders_by_id(
+    order_id:int,
+    session: Session=Depends(get_session)
+):
+    order_one = (
+        (session.query(orders.Order)
+        .filter(orders.Order.order_id == order_id)
+        .first())
+    )
     if order_one is None:
         raise HTTPException(status_code=404, detail="Order nor found")
     return order_one
