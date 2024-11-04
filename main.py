4bf2ec1d47f9b42d9a0c4009cdc1f1b7dfca8995
Popov Roman <popovrromma@gmail.com>
2024-11-04 20:15:03 +1000

    add code for connect with react

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api import router
 
-app = FastAPI(
-)
+app = FastAPI()
 app.include_router(router)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"]
+)
